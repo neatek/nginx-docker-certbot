cc04fdc53713a80d3e681f1b921502d983d24e64
Vladimir Zhelnov <neatek@icloud.com>
2024-05-22 12:31:14 +0300

    upd:install

--- install.sh
+++ install.sh
@@ -53,4 +53,14 @@ sudo apt-get install -y sshguard
 sudo systemctl start sshguard
 sudo systemctl enable sshguard
 
+# Установка и настройка UFW
+echo "Установка и настройка UFW..."
+sudo apt-get install -y ufw
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
+sudo ufw --force enable
+
 echo "Установка завершена!"
